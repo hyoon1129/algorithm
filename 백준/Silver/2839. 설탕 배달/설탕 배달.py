557360a2d6f7de540faa3b218caa4b2e753d7013
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2023-11-11 00:56:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+answer = 0
+
+while n > 0:
+    if n % 5 == 0:
+        answer += n//5
+        n = 0
+        break
+
+    n -= 3
+    answer += 1
+
+if n != 0:
+    answer = -1
+
+print(answer)
