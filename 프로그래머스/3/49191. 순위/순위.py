c93b82523b35d343bcc8772a93cc40d63efa7f2e
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-02-14 00:08:51 +0900

    [level 3] Title: 순위, Time: 3064.85 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -0,0 +1,46 @@
+def solution(n, results):
+    answer = []
+    win = {}
+    lose = {}
+    
+    for i in range(n):
+        win[i+1] = []
+        lose[i+1] = []
+    for result in results:
+        win[result[0]].append(result[1])
+            
+        lose[result[1]].append(result[0])
+        
+    for i in range(1,n+1):
+        if len(win[i]) + len(lose[i]) == n-1:
+            answer.append(i)
+    
+    for i in range(n):
+        for num in win:
+            for k in win[num]:
+                win[num] = list(set(win[num]+win[k]))
+                
+        for num in lose:
+            for k in lose[num]:
+                lose[num] = list(set(lose[num]+lose[k]))
+                
+    for i in range(1,n+1):
+        if len(win[i]) + len(lose[i]) == n-1 :
+            answer.append(i)
+    
+#     for i in range(n):
+#         cnt = 0
+#         for num in win:
+#             if len(win[num]) == i:
+#                 cnt += 1
+#                 node = num
+#         if cnt == 1:
+#             answer.append(node)
+#         else:
+#             break
+    
+#     for i in win:
+#         if len(win[i]) == n-1:
+#             answer.append(i)
+        
+    return len(set(answer))
