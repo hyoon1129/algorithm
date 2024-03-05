e38dc811f07c08abc6103cfb04537f530f7939d0
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-03-05 16:33:40 +0900

    [level 2] Title: 호텔 대실, Time: 80.98 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,26 @@
+def solution(book_time):
+    answer = 0
+
+    for t in book_time:
+        t[0] = minute(t[0])
+        t[1] = minute(t[1])
+    
+    book_time.sort(key = lambda x : (x[0], x[1]))
+    
+    room = []
+    
+    for book in book_time:
+        for i in range(len(room)):
+            if book[0] >= room[i] + 10 and book[1] <= minute("23:59"):
+                room[i] = book[1]
+                break
+        else:
+            room.append(book[1])
+                
+        # room.sort()
+    
+    
+    return len(room)
+
+def minute(s):
+    return int(s[:2])*60 + int(s[-2:])
