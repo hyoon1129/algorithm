602daef542e265ad54efb61714d6a822e8ee661a
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-09-27 15:06:48 +0900

    [level 1] Title: 추억 점수, Time: 1.29 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,9 +1,45 @@
 def solution(name, yearning, photo):
     answer = []
-    for li in photo:
+    dic = {}
+    for i in range(len(name)):
+        dic[name[i]] = yearning[i]
+    
+    for line in photo:
         result = 0
-        for nm in li :
-            if nm in name:
-                result += yearning[name.index(nm)]
+        for j in range(len(line)):
+            if line[j] in dic:
+                result += dic[line[j]]
         answer.append(result)
     return answer
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(name, yearning, photo):
+#     answer = []
+#     for li in photo:
+#         result = 0
+#         for nm in li :
+#             if nm in name:
+#                 result += yearning[name.index(nm)]
+#         answer.append(result)
+#     return answer
