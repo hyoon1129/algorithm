ea6f27eb13fc624467e02bc23904f509a9b93b82
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-11-06 22:52:34 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3468 ms, Memory: 274812 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,35 +1,37 @@
 import sys
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
-parent = [i for i in range(v+1)] # 각자 부모 노드를 자기 자신으로 초기화
 
-graph = []
-for i in range(e):
-    graph.append(list(map(int, input().split())))
+n, m = map(int, input().split())
 
-graph.sort(key = lambda x : x[2]) # 가중치를 기준으로 정렬
+matrix = []
+parent = [i for i in range(n+1)]
 
-
-def find_parent(node):
-    if node != parent[node]:
-        parent[node] = find_parent(parent[node])
-    return parent[node]
+for i in range(m):
+    matrix.append(list(map(int, input().split())))
 
 
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-def union(a,b):
-    a = find_parent(a)
-    b = find_parent(b)
-    if a < b: # 작은 노드로 부모 노드 갱신
+def union_parent(parent, a, b):
+    a = find_parent(parent,a)
+    b = find_parent(parent, b)
+    if a<b:
         parent[b] = a
     else:
         parent[a] = b
-    return
+
+
+matrix.sort(key = lambda x : x[2])
 
 answer = []
-for a,b,c in graph:
-    if find_parent(a) != find_parent(b): # 두 노드가 사이클을 발생시키지 않으면
-        answer.append(c)
-        union(a,b)
+
+for a, b, cost in matrix:
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        answer.append(cost)
+
 print(sum(answer)-max(answer))
