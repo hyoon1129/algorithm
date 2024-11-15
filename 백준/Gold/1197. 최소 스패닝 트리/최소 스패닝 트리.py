f7db79a93da97deb6f4699d6d95de3b30452e942
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-11-16 02:23:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 260 ms, Memory: 56384 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,37 +2,34 @@ import sys
 input = sys.stdin.readline
 
 v, e = map(int, input().split())
+parent = [i for i in range(v+1)] # 각자 부모 노드를 자기 자신으로 초기화
 
-matrix = []
-parent = [i for i in range(v+1)]
- 
+graph = []
 for i in range(e):
-    matrix.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
+
+graph.sort(key = lambda x : x[2]) # 가중치를 기준으로 정렬
 
 
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+def find_parent(node):
+    if node != parent[node]:
+        parent[node] = find_parent(parent[node])
+    return parent[node]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    
-    if a > b:
-        parent[b] = a
-    else:
+
+
+def union(a,b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b: # 작은 노드로 부모 노드 갱신
         parent[a] = b
+    else:
+        parent[b] = a
     return
 
-matrix.sort(key=lambda x: x[2])
-
-
 answer = 0
-
-for a, b, cost in matrix:
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent,a,b)
-        answer += cost
-
+for a,b,c in graph:
+    if find_parent(a) != find_parent(b): # 두 노드가 사이클을 발생시키지 않으면
+        answer += c
+        union(a,b)
 print(answer)
