d4b41e05a0302e98a9f2be2d0c85d9e24786966f
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2024-12-07 23:23:37 +0900

    [Silver II] Title: 주식, Time: 1940 ms, Memory: 153720 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,33 +1,27 @@
-import sys
-input = sys.stdin.readline
+# 1. 주식을 하나 산다
+# 2. 원하는 만큼 가지고 있는 주식을 판다
+# 3. 아무 것도 안한다
+
+# 날 별로 주식의 가격을 알려 줬을 때 최대 이익이 얼마나 되는지 계산
+# 계속 감소하면 최대 이익 0
 
 t = int(input())
-li = []
-for i in range(t):
-    answer = 0
-    n = int(input())
-    stock = list(map(int, input().split()))
-    stock.reverse()
-    now_max = 0
-    for j in range(len(stock)):
-        if stock[j] > now_max:
-            now_max = stock[j]
-        else:
-            answer += now_max - stock[j]
-    li.append(answer)
-print(*li, sep = "\n")
-'''
-처음 제출한 코드는 입력이
-1
-4
-6 4 7 2 일 때
-넘어감 -> 구매 -> 판매 -> 넘어감
-이어서 6을 구매하지 않아 3이 출력된다. 
-첫날과 둘째날 6, 4를 구매하고 3일차에 팔아서 최대 이익 4를 만들어야 한다.
+answer = []
 
-stock을 뒤에서부터 max를 찾아 그 전날 주가보다 max가 크면 
-answer += max - 전 날 주가
-를 하고
-그 전날 주가가 max보다 크면
-max를 그 전날 주가로 업데이트 한다.
-'''
+def getProfit(n, day):
+    max_price = 0
+    profit = 0
+    for price in day[::-1]:
+        if price > max_price:
+            max_price = price
+        else:
+            profit += max_price - price
+    return profit
+
+for i in range(t):
+    n = int(input())
+    day = list(map(int, input().split()))
+    answer.append(getProfit(n, day))
+
+for i in range(t):
+    print(answer[i])
