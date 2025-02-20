ca2b97670e6bf74a442031fe9a0e176fb5ab2903
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-02-21 01:00:20 +0900

    [Gold IV] Title: 수 묶기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,43 +1,38 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-negative = [] # 음수 저장
-positive = [] # 양수 저장
-zero = False # 0이 있으면 false, 없으면 true
-
+negative = []
+positive = []
+zero = False
 for i in range(n):
-    a = int(input())
-    if a > 0:
-        positive.append(a)
-    elif a == 0:
+    num  = int(input())
+    if num > 0:
+        positive.append(num)
+    elif num == 0:
         zero = True
     else:
-        negative.append(a)
+        negative.append(num)
 
-positive.sort(reverse = True)
+positive.sort(reverse=True)
 negative.sort()
+# 0은 무시.
+# 음수는 음수 끼리 곱하기.
+# li length 가 1이면 그대로 출력
+# 1 1 이면 2
+
 answer = 0
 
-if len(negative)%2 == 1: # 음수 개수가 홀수개면
-    for i in range(0, len(negative)-1, 2):\
-        answer += negative[i]*negative[i+1]
-    if not zero: # 0이 dqjtdmaus
+for i in range(0, len(negative) - 1, 2):
+    answer += negative[i] * negative[i + 1]
+
+if len(negative)%2 == 1:
+    if not zero:
         answer += negative[-1]
 
-else: # 음수 개수가 짝수개면
-    for i in range(0, len(negative)-1, 2):
-        answer += negative[i]*negative[i+1]
-
-
-for i in range(0, len(positive) - 1, 2):
-    if positive[i] + positive[i+1] < positive[i] * positive[i + 1]:
-        answer += positive[i] * positive[i + 1]
+for i in range(0, len(positive)-1, 2):
+    if positive[i] + positive[i+1]  < positive[i] * positive[i+1]:
+        answer += positive[i] * positive[i+1]
     else:
-        answer +=  positive[i] + positive[i+1]
-
-if len(positive)%2 == 1:
+        answer += positive[i] + positive[i+1]
+if len(positive) % 2 == 1:
     answer += positive[-1]
-
 
 print(answer)
