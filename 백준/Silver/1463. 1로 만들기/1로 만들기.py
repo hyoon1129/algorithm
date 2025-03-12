b5862e3b65536019b2b0aec3835474559a9dfb55
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-03-12 09:45:36 +0900

    [Silver III] Title: 1로 만들기, Time: 544 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,6 +1,12 @@
+# 3으로 나누어 떨어지면 3으로 나눔
+# 2로 나누어 떨어지면 2로 나눔
+# 1을 뺌
+
+
 n = int(input())
 
 dp = [0 for i in range(n+3)]
+
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
@@ -9,11 +15,10 @@ for i in range(4, n+1):
     nanugi3 = 10**9
     nanugi2 = 10**9
 
-    if i%3 == 0:
+    if i % 3 == 0:
         nanugi3 = dp[i//3]
-    if i%2 == 0:
+    if i % 2 == 0:
         nanugi2 = dp[i//2]
 
     dp[i] = min(nanugi3, nanugi2, dp[i-1]) + 1
-
 print(dp[n])
