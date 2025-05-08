06d8500d81b4f9a98203c29cf7d399b79d66d4f6
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-05-08 16:34:53 +0900

    [Silver V] Title: 문서 검색, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -4,14 +4,14 @@ input = sys.stdin.readline
 writing = input().strip()
 word = input().strip()
 
-wlen = len(word)
-
+wlen = len(word)-1
 answer = 0
 idx = 0
-while idx < len(writing):
-    if writing[idx:idx+wlen] == word:
+
+while len(writing) >= len(word) :
+    if word in writing:
         answer += 1
-        idx += wlen
+        writing = writing.replace(word, '0', 1)
     else:
-        idx += 1
+        break
 print(answer)
