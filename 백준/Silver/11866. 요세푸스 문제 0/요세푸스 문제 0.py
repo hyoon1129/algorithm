2fc459a74a61a5aab239ee7b8cdb3189be4bab3a
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-05-28 10:33:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+num = [i for i in range(1, n+1)]
+
+idx = 0
+
+answer = []
+
+while num:
+    idx = (idx+k-1) % len(num)
+    answer.append(num.pop(idx))
+
+print("<" + ", ".join(map(str, answer)) + ">")
