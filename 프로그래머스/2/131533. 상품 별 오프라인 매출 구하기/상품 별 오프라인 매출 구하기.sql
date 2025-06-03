1a072250abc7dda9001721eadd0223fb9f678e7f
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-06-03 20:37:44 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,6 @@
+select p.PRODUCT_CODE, sum(p.PRICE * o.SALES_AMOUNT) AS 'SALES'
+from PRODUCT p
+join OFFLINE_SALE o
+on p.PRODUCT_ID = o.PRODUCT_ID
+group by p.PRODUCT_CODE
+order by SALES DESC, p.PRODUCT_CODE
