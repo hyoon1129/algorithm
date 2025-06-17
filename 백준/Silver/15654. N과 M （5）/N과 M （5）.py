267a76127213b21d7f3fc3e6c537aa43feaea8e7
hyoon1129 <81309533+hyoon1129@users.noreply.github.com>
2025-06-17 11:36:51 +0900

    [Silver III] Title: N과 M (5), Time: 108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -3,15 +3,7 @@ n, m = map(int, input().split())
 
 li = list(map(int, input().split()))
 li.sort()
-pmt = permutations(li, m)
-answer = []
 
-for p in pmt:
-    result = ""
-    for i in p:
-        result += str(i)
-        result += ' '
-    answer.append(result)
 
-for i in range(len(answer)):
-    print(answer[i])
+for p in permutations(li, m):
+    print(" ".join(map(str, p)))
